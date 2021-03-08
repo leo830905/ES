1f9323df109e5d27671e9025fe76b2521b6eb9a8
Leo <leo19940905@gmail.com>
2021-03-08 14:03:24 +0800

    6

--- src/17.export-import.js
+++ src/17.export-import.js
@@ -4,3 +4,5 @@ const a1 = [2, 4, 6];
 const a2 = [1, 3, 5];
 
 export { a1, a2 };
+
+console.log(a1.join("-"));
--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import my, { a1, a2 } from "./17.export-import";
+import { a1 as aaa } from "./17.export-import"
 const info = document.querySelector("#info");
 
 
@@ -6,6 +7,7 @@ const info = document.querySelector("#info");
 info.innerHTML += my(5) + "<br>";
 info.innerHTML += a1 + "<br>";
 info.innerHTML += a2 + "<br>";
+console.log(a1 === aaa);  //true
 
 
 
